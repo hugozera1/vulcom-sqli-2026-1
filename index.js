594c3fc3aeb47053e3a0904fde7e5fd4876a615a
Hugo Rodrigues <161486790+hugozera1@users.noreply.github.com>
2026-03-28 13:32:05 +0000

    (26/03) Exploração da vulnerabilidade SQL Injection

--- index.js
+++ index.js
@@ -29,9 +29,9 @@ app.post('/login', (req, res) => {
     const { username, password } = req.body;
     
     // CONSULTA SQL VULNERÁVEL 🚨
-    const query = `SELECT * FROM users WHERE username = '${username}' AND password = '${password}'`;
+    const query = `SELECT * FROM users WHERE username = ? AND password = ?`;
     
-    db.all(query, [], (err, rows) => {
+    db.all(query, [username, password], (err, rows) => {
         if (err) {
             return res.send('Erro no servidor');
         }
